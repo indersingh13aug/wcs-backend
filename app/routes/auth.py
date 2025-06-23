5755d82775a4c2e64696e69510833edddf3ece09
Inder <kumar.inder.com>
2025-06-23 10:27:18 +0530

    update

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -54,6 +54,7 @@ def get_user(username):
     import sqlite3
     DB_PATH = "erp.db"
     conn = sqlite3.connect(DB_PATH)
+    logger.info(conn)
     cursor = conn.cursor()
     logger.info("get_:",cursor)
 
